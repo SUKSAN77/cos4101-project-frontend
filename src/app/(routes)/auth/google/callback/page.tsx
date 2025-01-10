94cc716336a5147a09a6d582f34efb429bcf7a4f
STACiA <69411527+staciax@users.noreply.github.com>
2025-01-10 23:36:07 +0700

    display data from google auth from json

--- src/app/(routes)/auth/google/callback/page.tsx
+++ src/app/(routes)/auth/google/callback/page.tsx
@@ -5,15 +5,5 @@ import { useSearchParams } from "next/navigation";
 export default function Page() {
 	const searchParams = useSearchParams();
 
-	const hd = searchParams.get('hd');
-	console.log('hd', hd);
-
-	const code = searchParams.get('code');
-	console.log('code', code);
-
-	return (
-		<div>
-			<p>Redirecting...</p>
-		</div>
-	);
+	return <pre>{JSON.stringify(Object.fromEntries(searchParams), null, 2)}</pre>;
 }
