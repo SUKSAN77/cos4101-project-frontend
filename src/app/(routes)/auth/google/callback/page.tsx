249c1985ddbf2751b5a2065a0b2c277b1f4d536d
STACiA <69411527+staciax@users.noreply.github.com>
2025-01-12 14:46:14 +0700

    update google auth

--- src/app/(routes)/auth/google/callback/page.tsx
+++ src/app/(routes)/auth/google/callback/page.tsx
@@ -1,9 +1,30 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { useRef, useEffect } from "react";
 
 export default function Page() {
 	const searchParams = useSearchParams();
+	const called = useRef(false);
+	const code = searchParams.get("code");
+	// const state = searchParams.get("state");
 
-	return <pre>{JSON.stringify(Object.fromEntries(searchParams), null, 2)}</pre>;
+	useEffect(() => {
+		(async () => {
+			try {
+				if (called.current) return;
+				called.current = true;
+				if (code) {
+					const response = await fetch(
+						`http://localhost:8000/api/v1/auth/google/callback?code=${code}`,
+					);
+					console.log(response);
+				}
+			} catch (err) {
+				console.error(err);
+			}
+		})();
+	}, [code]);
+
+	return <div>Redirecting...</div>;
 }
