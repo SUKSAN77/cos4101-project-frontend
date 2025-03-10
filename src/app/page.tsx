c1f1b44f4943c34ca9319db8eccb1205f7cea7ce
Suksan <kornkung_080@hotmail.com>
2025-03-11 05:42:44 +0700

    chore: update navbar in root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
-import { Login } from "./(routes)/login/page";
+import { BarChartComponent } from "@/components/chart-bar-multiple";
+import { PieChartComponent } from "@/components/chart-pie-donut-text";
+import EquipmentViewer from "@/components/EquipmentViewer";
+import Navbar from "@/components/Navbar";
 
 export default function App() {
     return (
-        <>
-            <Login />;
-        </>
+        <div className="container mx-auto px-4">
+            <Navbar />
+            <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+                <div className="col-span-1 md:col-span-2">
+                    <div className="rounded-lg bg-white p-6 shadow-md">
+                        <EquipmentViewer />
+                    </div>
+                </div>
+                <div className="rounded-lg bg-white p-6 shadow-md">
+                    <h2 className="mb-4 text-2xl font-semibold">
+                        สถิติประเภทอุปกรณ์
+                    </h2>
+                    <PieChartComponent />
+                </div>
+                <div className="rounded-lg bg-white p-6 shadow-md">
+                    <h2 className="mb-4 text-2xl font-semibold">
+                        สถิติการใช้งาน
+                    </h2>
+                    <BarChartComponent />
+                </div>
+            </div>
+        </div>
     );
 }
