35ef4a8023463e5c29da241639080487d6b4307a
STACiA <69411527+staciax@users.noreply.github.com>
2025-03-10 15:14:32 +0700

    chore: remove unsused

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-    env: {
-        REACT_APP_API_URL: process.env.REACT_APP_API_URL,
-    },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
--- test1.tsx
+++
@@ -1,5 +0,0 @@
-import { test2 } from "./test2";
-import x from "./test2";
-
-x();
-test2();
