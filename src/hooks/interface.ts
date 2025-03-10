3bab67dfd6d7199ec294cc34acf6f6e5b0d2260b
STACiA <69411527+staciax@users.noreply.github.com>
2025-03-10 14:30:48 +0700

    Update interface.ts

--- src/hooks/interface.ts
+++ src/hooks/interface.ts
@@ -1,3 +1,17 @@
-import { GetApiV1UsersMeResponses } from "@/client";
+import {
+    GetApiV1CategoriesByIdResponse,
+    GetApiV1RoomsByIdResponse,
+    GetApiV1UsersByIdResponse,
+    GetApiV1UsersMeResponse,
+    PostApiV1CategoriesResponse,
+} from "@/client";
 
-export type User = GetApiV1UsersMeResponses[200];
+export type User = GetApiV1UsersByIdResponse;
+
+export type UserMe = GetApiV1UsersMeResponse;
+
+export type Room = GetApiV1RoomsByIdResponse;
+
+export type Category = GetApiV1CategoriesByIdResponse;
+
+export type CreateCategory = PostApiV1CategoriesResponse;
